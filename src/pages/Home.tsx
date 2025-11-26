52cf9dbcc3b84dd38aaeb386cc4bbbb91326f160
KoneJ <kbvv67890@gmail.com>
2025-11-26 13:12:39 +0900

    Fix: 존재하지 않는 컴포넌트 import 경로 수정 (Banner -> MainBanner, TopBar -> HomeHeader)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import Banner from '@/components/Home/Banner';
-import TopBar from '@/components/TopBar';
+import MainBanner from '@/components/Home/MainBanner';
+import HomeHeader from '@/components/Home/Header';
 
 export default function HomePage() {
   return (
     <div className="min-h-screen w-full max-w-mobile mx-auto">
-      <TopBar />
-      <Banner />
+      <HomeHeader />
+      <MainBanner />
       <main className="px-4 py-6">
         <div className="flex flex-col gap-4">
           {/* 컨텐츠 추가 */}
